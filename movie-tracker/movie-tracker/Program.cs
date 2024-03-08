4750b9df74a7c50309b50bfdba7df7ee1f919a8a
GarciaMarkAngelo-BSIT32E1 <160812645+GarciaMarkAngelo-BSIT32E1@users.noreply.github.com>
2024-03-08 19:58:17 +0800

    Naayos na

--- movie-tracker/movie-tracker/Program.cs
+++ movie-tracker/movie-tracker/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using movie_tracker.Data;
+
 namespace movie_tracker
 {
     public class Program
@@ -8,6 +11,8 @@ namespace movie_tracker
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             var app = builder.Build();
 
