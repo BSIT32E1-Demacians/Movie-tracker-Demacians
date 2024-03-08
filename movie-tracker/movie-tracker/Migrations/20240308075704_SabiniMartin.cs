e36c90b1ee791d69e63444990e4f49e92837bbfa
GarciaMarkAngelo-BSIT32E1 <160812645+GarciaMarkAngelo-BSIT32E1@users.noreply.github.com>
2024-03-08 17:56:36 +0800

    Pacheck ung Movie ayaw gumana

---
+++ movie-tracker/movie-tracker/Migrations/20240308075704_SabiniMartin.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GroupActivity.Migrations
+{
+    /// <inheritdoc />
+    public partial class SabiniMartin : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Quotes",
+                columns: new[] { "QuoteId", "Author", "QuoteText" },
+                values: new object[] { 11, "Literally Everyone Except Gojo Satoru", "Nah, I'd Win." });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Quotes",
+                keyColumn: "QuoteId",
+                keyValue: 11);
+        }
+    }
+}
---
+++ movie-tracker/movie-tracker/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace movie_tracker.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int MovieId { get; set; }
+        [Required]
+        [MaxLength(63)]
+        public string Title { get; set; }
+        [Range(1900,2100)]
+        public int ReleaseYear { get; set; }
+        public string Genre { get; set; }
+    }
+}
